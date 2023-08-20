9e287cf784a7fdd06f89c13cc084e811203a0279
Alperen Karacor <blackcor71@gmail.com>
2023-08-20 13:43:12 +0300

    v2

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -2,10 +2,15 @@ public class Class01 {
 
     public static void main(String[] args) {
 
+//yesil ise gecici alanda
 
-        //Versiyon 1 kodlari
-
+        //Versiyon yuklemek icin icin git commit -m""
+        //WS git add . yazarak SA ya gonderiyor git commit yazarak repositorye gonderilir
+         //Versiyon 1 kodlari
         System.out.println("Versiyon1");
+
+        //Versiyon 2 Kodlari
+        System.out.println("versiyon2");
 
 
     }
